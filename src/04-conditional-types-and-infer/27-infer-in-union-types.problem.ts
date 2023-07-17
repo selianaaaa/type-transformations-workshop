308c8b9eea1b5b63ed50a1c61194d3157ccb74b7
Yulia <cany96@bk.ru>
2023-07-17 11:49:34 +0200

    feat: new changes

--- src/04-conditional-types-and-infer/27-infer-in-union-types.problem.ts
+++ src/04-conditional-types-and-infer/27-infer-in-union-types.problem.ts
@@ -1,10 +1,10 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 const parser1 = {
   parse: () => 1,
 };
 
-const parser2 = () => "123";
+const parser2 = () => '123';
 
 const parser3 = {
   extract: () => true,
@@ -15,5 +15,5 @@ type GetParserResult<T> = unknown;
 type tests = [
   Expect<Equal<GetParserResult<typeof parser1>, number>>,
   Expect<Equal<GetParserResult<typeof parser2>, string>>,
-  Expect<Equal<GetParserResult<typeof parser3>, boolean>>,
+  Expect<Equal<GetParserResult<typeof parser3>, boolean>>
 ];
